573d41e42ba9fbb1dd88d02a835b5d8bafe8e82c
AnnaBoeghLarsen <anbo40565@edu.ucl.dk>
2023-06-06 13:25:18 +0200

    Header og kontakt

--- java.js
+++ java.js
@@ -1,1 +1,21 @@
-
+function validation(){
+    var nameV = document.getElementById('navn').value;
+    var mailV = document.getElementById('email').value;
+  
+    var errormsgV = document.getElementById('error_message');
+    var msgV =["Venligst skriv navn", "Venligst skriv email"];
+  
+  
+    if(nameV.length <5 ){
+        errormsgV.innerHTML = msgV[0];
+        return false;
+    }
+  
+    if(mailV.length <6 ){
+        errormsgV.innerHTML = msgV[1];
+        return false;
+    }
+    else{
+     alert("Tak - din mail er blevet sendt. Tjek din mail for yderligere info");
+    }
+  }
